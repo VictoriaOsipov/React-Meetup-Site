956cf033228fc30295693d72257ea3f68ccc58f7
VictoriaOsipov <victoria.osipov@gmail.com>
2021-05-25 15:34:53 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <h1>My Todos</h1>
+      <div>
+        <h2>TITLE</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
