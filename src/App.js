28448d09154a563681d0bd0dbe1e74b34057d7c3
VictoriaOsipov <victoria.osipov@gmail.com>
2021-06-21 17:38:06 +0300

    update layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Route, Switch, Li } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import NewMeetupPage from './pages/NewMeetup'
 import FavoritesPage from './pages/Favorites'
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout'
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>    
         makes only one of the routes active. without it Route uses "if includes" and will show all on them 
         <Route path='/' exact={true}> 
@@ -22,7 +21,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
