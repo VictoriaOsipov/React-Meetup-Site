15c628bd0af7729acaed65dac75ab1380c8ff18d
VictoriaOsipov <victoria.osipov@gmail.com>
2021-06-21 12:28:59 +0300

    restart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
+
 function App() {
   return (
-    <div className="App">
-      <h1>My Todos</h1>
-      <div>
-        <h2>TITLE</h2>
-        <div>
-          <button>Delete</button>
-        </div>
-      </div>
+    <div>
+  
     </div>
   );
 }
